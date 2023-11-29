51b1a607f3b23c1efe955f8ab7ecc5951947ed6a
ismael <ismael.castillo@omegasoftve.com>
2023-11-29 10:37:10 -0400

    last changes

--- controllers/carrera_universitaria.js
+++ controllers/carrera_universitaria.js
@@ -3,6 +3,14 @@ const db = require("../models");
 const CarreraUniversitaria = db.carrera_universitaria;
 const Op = db.Sequelize.Op;
 
+const recordValidations = (data) =>  {
+    let objReturn = {'status': 'success', 'data': {}, 'msg': ''};
+    if(data.nombre.trim() == "") {
+        objReturn = {'status': 'failed', 'data': {}, 'msg': 'Se debe definir un nombre para la categoría de noticia.'};
+        return objReturn;
+    }
+    return objReturn;
+  }
 
 exports.findAll = (req, res) => {
     const parameter = req.query.parameter;
@@ -38,5 +46,90 @@ exports.findAll = (req, res) => {
         res.status(500).send({
         message: "Ocurrió un error durante la busqueda de la carrera universitaria."
         });
+    });
+};
+
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+    CarreraUniversitaria.findOne({where: {id: id}, paranoid: true})
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({message: `No se pudo encontrar el registro.`});
+      }
+    })
+    .catch(err => {
+      res.status(500).send({message: "Ocurió un error inesperado... Intentelo mas tarde."});
+    });
+};
+
+exports.create = async (req, res) => {
+    const bodyData = req.body;
+    const validations= recordValidations(bodyData);
+    const errorMessage = "Ocurrió un error inesperado al intentar crear el registro.";
+
+    if(validations.status != 'success') {
+      res.status(400).send({message: validations.msg});
+    }
+
+    CarreraUniversitaria.create({
+      nombre: bodyData.nombre,
+      nombre_pnf: bodyData.nombre_pnf,
+      codigo_pnf: bodyData.codigo_pnf,
+      pertenece_uni: true
+    })
+    .then(recordRes => {
+        bodyData.id = recordRes.dataValues.id;
+        res.status(200).send(bodyData);
+    }).catch(err => {
+      res.status(500).send({message: errorMessage});
+    });
+};
+
+exports.update = async (req, res) => {
+    const id = req.params.id;
+    let bodyData = req.body;
+    let validations = recordValidations(bodyData);
+    const errorMessage = "Ocurrió un error inesperado al intentar actualizar el registro.";
+
+    if(validations.status != 'success') {
+      res.status(400).send({message: validations.msg});
+    }
+
+    CarreraUniversitaria.update({
+        nombre: bodyData.nombre,
+        nombre_pnf: bodyData.nombre_pnf,
+        codigo_pnf: bodyData.codigo_pnf
+    }, {where: {id: id}})
+    .then(recordRes => {
+      res.send({message: "El registro fue actualizado satisfactoriamente!!", data: bodyData});
+    }).catch(err => {
+      res.status(500).send({message: errorMessage});
+    });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+    CarreraUniversitaria.destroy({
+      where: { id: id },
+      individualHooks: true
+    })
+    .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "El registro fue eliminado exitosamente!!"
+          });
+        } else {
+          res.send({
+            message: `No se pudo eliminar el registro.`
+          });
+        }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({
+        message: "No se pudo eliminar el registro"
+      });
     });
 };
--- routes/carrera_universitaria.js
+++ routes/carrera_universitaria.js
@@ -5,6 +5,14 @@ module.exports = app => {
 
     //busqueda
     router.get("/", carrera_universitaria.findAll);
+
+    router.post("/", carrera_universitaria.create);
+
+    router.get("/:id", carrera_universitaria.findOne);
+
+    router.put("/:id", carrera_universitaria.update);
+    
+    router.delete("/:id", carrera_universitaria.delete);
   
     app.use('/api/carrera_universitaria', functions.verifyToken, router);
 };
